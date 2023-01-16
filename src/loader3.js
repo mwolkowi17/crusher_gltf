2048f17bd15a77be9afb64363fbceea116e159a2
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2023-01-16 10:43:30 +0100

    new model

--- src/loader3.js
+++ src/loader3.js
@@ -6,7 +6,7 @@ export let mixer= new THREE.AnimationMixer();
 export let action;
 export const gltfLoader3 = new GLTFLoader()
     //gltfLoader2.load('./przesiewacz1.glb', (gltf) => {
-    gltfLoader3.load('./lacznik1.glb', (gltf) => {
+    gltfLoader3.load('./tloczny3.glb', (gltf) => {
       const root = gltf.scene;
       const anim = gltf.animations;
       scene.add(root); //co zrobic z tym?
